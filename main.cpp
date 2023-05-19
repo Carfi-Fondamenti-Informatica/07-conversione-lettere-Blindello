f72d98e3cb79072b5d48424b91b53dcb24ebef7c
Blindello <129384662+Blindello@users.noreply.github.com>
2023-05-19 16:34:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#inlcude "Lib.h"
 
 int main(){
-  return 0;
+  char n;
+  cin>>n;
+  char x=0;
+  x=funzione1(n);
+  if(x==0) {
+   cout<<"errore"<<endl;
+  }else 
+    cout<<x<<endl;
+    return 0;
 }
