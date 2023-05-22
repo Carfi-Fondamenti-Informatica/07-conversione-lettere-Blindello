aaf764ebd0ccd6dca0ed1dea1934c70bf2a9db8e
Blindello <129384662+Blindello@users.noreply.github.com>
2023-05-22 15:37:49 +0200

    Update libreria.cpp

--- libreria.cpp
+++ libreria.cpp
@@ -1,11 +1,11 @@
 #include "Libreria.h"
 
 bool funzione1 (char n) {
-  if (n>=65 && n<=90) {
-    n=(n+32);
-  } else if (n>=97 && n<=122) {
-    n=(n-32);
-  } else {
-    return false;
-  }
+    if (n>=65 && n<=90) {
+        n=(n+32);
+    } else if (n>=97 && n<=122) {
+        n=(n-32);
+    } else {
+        return false;
+    }
 }
